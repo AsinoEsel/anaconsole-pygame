6e8ba2c945f9753cdb68c672db30cb8c90d9bcc8
AsinoEsel <lerefff@gmail.com>
2025-05-26 20:17:46 +0200

    Change variable name to be more general

--- anaconsole/assets/__init__.py
+++ anaconsole/assets/__init__.py
@@ -4,11 +4,11 @@ import io
 
 def load_file_stream(file_name: str, package: str = 'anaconsole.assets') -> io.BytesIO:
     """
-    Loads a font file from a package and returns a BytesIO stream suitable for pygame.font.Font.
+    Loads a font file from a package and returns a BytesIO stream, allowing packed files to be read.
 
     :param file_name: The name of the file (e.g., "my_font.ttf").
     :param package: The dotted path to the package containing the file.
     :return: BytesIO object containing the font data.
     """
-    with importlib.resources.open_binary(package, file_name) as font_file:
-        return io.BytesIO(font_file.read())
+    with importlib.resources.open_binary(package, file_name) as file:
+        return io.BytesIO(file.read())
